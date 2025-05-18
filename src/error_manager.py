46f728c1f89e3e5dadef9e6eb705f9fb0a5cd63d
Fannytas <fannybrcl@gmail.com>
2025-05-18 15:42:55 +0200

    maj chemin dico

--- src/error_manager.py
+++ src/error_manager.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import json
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DICO_PATH = os.path.join(BASE_DIR, "data", "dico_error.json")
 ERROR_LOG_FILE = None
 
